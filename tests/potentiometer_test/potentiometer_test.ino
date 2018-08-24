dfc7b9a7045caf55a17edd8ed2eb21ca22dfd27d
Jacob <jacob.drummerboy25@hotmail.co.uk>
2018-08-24 15:24:00 +0100

    initial demo with potentiometer

---
+++ tests/potentiometer_test/potentiometer_test.ino
@@ -0,0 +1,19 @@
+//Constants:
+const int potPin = A0; //pin A0 to read analog input
+
+//Variables:
+int value; //save analog value
+
+void setup(){
+  pinMode(potPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  value = analogRead(potPin);
+  value = map(value, 0, 1023, 0, 360);
+  Serial.println(value);
+}
+
+
+                           
